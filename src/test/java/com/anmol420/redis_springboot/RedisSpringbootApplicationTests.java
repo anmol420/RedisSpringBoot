ba2b43487553ab1932dcce68d9fff47245e64b44
anmol420 <anmolanand2k15@gmail.com>
2025-06-17 23:50:54 +0530

    Initial Commit

---
+++ src/test/java/com/anmol420/redis_springboot/RedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol420.redis_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
