a67a0eb8a44ce68d1fc5233438e483d3d0fa093f
anmol420 <anmolanand2k15@gmail.com>
2025-06-18 20:31:38 +0530

    Added Redis Configurations

--- src/main/java/com/anmol420/redis_springboot/RedisSpringbootApplication.java
+++ src/main/java/com/anmol420/redis_springboot/RedisSpringbootApplication.java
@@ -2,8 +2,10 @@ package com.anmol420.redis_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisSpringbootApplication {
 
 	public static void main(String[] args) {
